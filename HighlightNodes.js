6295321f157c69170120ebf43c679e33607cb4fa
Marshall-T <mjtshopper@gmail.com>
2016-10-11 21:28:41 -0500

    Chapter 6 Project 1 complete. red error flags use parent backgrounds, as the book shows, not just field backgrounds. The listeners are triggered with focus instead of key down and the checkbox listener is triggered by onchange.  Works well in Safari, Firefox, Chrome, and Opera.

--- HighlightNodes.js
+++ HighlightNodes.js
@@ -2,18 +2,27 @@
     CIP 228
     Chapter 6 Project 1
 */
-txt = "";
+var txt = "", txt2 = "";
 
 var aList = document.getElementsByTagName("*");
+var nodeArray = new Array();
+var arrayOffset = 0;
 
 for (i = 0; i < aList.length; i++) {
     if (i > 4) {
-        txt = "Name = " + aList[i].nodeName + " Type = " + aList.nodeType;
-        console.log (txt);
-        console.log (aList[i]);
+//        if (aList[i].nodeType === 3 ) {
+            //  Add the node to an array and increment array offset
+            nodeArray.push = aList[i].nodeName;
+            arrayOffset++;
+    txt = "Name = " + aList[i].nodeName + " Type = " + aList[i].nodeType;
+    console.log (txt);
+    txt2 = "Offset = " + arrayOffset + " Array = " + nodeArray[arrayOffset];
+    console.log (txt2);
+//    console.log (aList[i]);
+//        }
     }
 }
-
+//Loop through node array and append the child node with the node type, document.createElement
 
 /*
 console.log ("* aList");
--- chapter06-project01.js
+++ chapter06-project01.js
@@ -4,51 +4,59 @@
     Chapter 6 Project 1
 */
 
-document.getElementById("mainForm").onsubmit = function(e) {
-var hiLite = document.getElementById("hiLite").value;
-//  var rectangleHiLite = document.getElementById("rectangleHiLite").value;
+window.onload = function () {
+    //  cashe form status
+    var title = document.getElementById("title");
+    var description = document.getElementById("description");
+    var aCkBox = document.getElementById("aCkBox");
+    
+    //  activated title box, clear it for typing
+    title.onfocus = function() {
+        removeRed(title.parentNode);
+        }
+    //  activated description box, clear it for typing
+    description.onfocus = function(e) {
+        removeRed(description.parentNode);
+        }
+    //  changed checkBox box, clear it
+    aCkBox.onchange = function(e) {
+        removeRed(aCkBox.parentNode);
+        }
 
-    if ( (hiLite == null) || (hiLite == "") ) {
-        e.preventDefault();
-        alert ("You must enter Title, Description, & √ License agreement");
-
-        var colorTag = document.getElementById("hiLite");
-        colorTag.className = "red";
+        //  this does all the clearing of the red
+    var removeRed = function (element) {
+        element.classList.remove("red");
     }
 
-    document.getElementById("hiLite").onkeydown = function(e) {
-        var colorTag = document.getElementById("hiLite");
-        colorTag.className = "required";
-        
+    //  this set background to red (called if empty)
+    var applyRed = function (element) {
+        element.classList.add ("red");
     }
 
-//  console.log (colorTag)
+//  the book shows not just the field but also the parent background as
+//  red also needed to set the fields to transparent to get the red to
+//  come through in a single step
+    document.getElementById("mainForm").onsubmit = function() {
+        var error = false;
 
- }
- 
-
-document.getElementById("mainForm").onsubmit = function(e) {
-  var rectangleHiLite = document.getElementById("rectangleHiLite").value;
-
-    if ( (rectangleHiLite == null) || (rectangleHiLite == "") ) {
-        e.preventDefault();
-        alert ("You must enter √ the License agreement");
-
-        var rectangleColor = document.getElementById("rectangleHiLite");
-        rectangleColor.className = "red";
-
+        //  check for empty title field and red it
+        if (title.value === null || title.value === "") {
+            error = true;
+            applyRed (title.parentNode);
+        }
+        //  check for empty description field and red it
+        if (description.value === null || description.value.trim() === "") {
+            error = true;
+            applyRed (description.parentNode);
+        }
+        //  check for empty check box and red it
+        if (aCkBox.checked === false) {
+            error = true;
+            applyRed (aCkBox.parentNode);
+        }
+        //  send results to html
+        return !error;
     }
+}
 
-    document.getElementById("rectangleHiLite").onkeydown = function(e) {
-        var rectangleColor = document.getElementById("rectangleHiLite");
-        rectangleColor.className = "required";
-    }
 
-//  console.log (colorTag)
-
- }
-
-/*
-console.log (mainForm);
-console.log(typeof mainForm);
-*/
