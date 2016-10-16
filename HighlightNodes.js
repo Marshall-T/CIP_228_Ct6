58b796c41800250ee0fa96afdeca85e93e41fa8f
Marshall-T <mjtshopper@gmail.com>
2016-10-16 05:40:05 -0500

    ct 6 p2 trying to access the nodes to append or create new nodes can't seem to get the correct method

--- HighlightNodes.js
+++ HighlightNodes.js
@@ -2,33 +2,43 @@
     CIP 228
     Chapter 6 Project 1
 */
-var txt = "", txt2 = "";
 
-var aList = document.getElementsByTagName("*");
-var nodeArray = new Array();
-var arrayOffset = 0;
+window.onload = function () {
+    var txt = "";
+    
+    var aList = document.getElementsByTagName("*");
+    
+    var nodeArray = new Array();
 
-for (i = 0; i < aList.length; i++) {
-    if (i > 4) {
-//        if (aList[i].nodeType === 3 ) {
-            //  Add the node to an array and increment array offset
-            nodeArray.push = aList[i].nodeName;
-            arrayOffset++;
-    txt = "Name = " + aList[i].nodeName + " Type = " + aList[i].nodeType;
-    console.log (txt);
-    txt2 = "Offset = " + arrayOffset + " Array = " + nodeArray[arrayOffset];
-    console.log (txt2);
-//    console.log (aList[i]);
-//        }
+    for (i = 0; i < aList.length; i++) {
+        if (i > 4) {
+                //  Add the node to an array and increment array offset
+                nodeArray.push (aList[i].nodeName);
+                if (aList[i].hasAttributes()) {
+                    for (j = 0; j < aList[i].childNodes.length; j++) {
+//                        if (aList[i].childNodes[j].nodeType === 3 ) {
+  //                          console.log ('****');
+    //                        console.log (aList[i].childNodes[j].nodeType);
+      //                      console.log (aList[i].childNodes[j].nodeName);
+        //                }
+                    }
+                }
+//        txt = "Name " + i + " = " + aList[i].nodeName + " Type = " + aList[i].nodeType;
+//        console.log (txt);
+        }
     }
+
+// Loop through node array and append the child node with the node type, document.createElement
+
+    //  trying to find a node ?
+    for (var i = 0; i < nodeArray.length; i++){
+        //  trying to find a node ?
+        txt = document.getElementsBytagName + " - " + nodeArray[i];
+        console.log (txt);
+//        document.getElementsByName.appendChild(nodeArray[i]);
+    }
+    
+        console.log ('**X**');
+        console.log (nodeArray);
 }
-//Loop through node array and append the child node with the node type, document.createElement
-
-/*
-console.log ("* aList");
-console.log (aList[5].innerHTML);
-console.log (aList.length);
-console.log (aList.item);
-/*
-
-*/
+// Loop through node array and append the child node with the node type, document.createElement
